969c85e9e8121bf218d9be356340e712f59b12f5
johnboeira <johnboeira@gmail.com>
2025-01-25 01:55:37 -0300

    feat: Usando pacote externo

--- 1 - pacotes/auxiliar/auxiliar.go
+++ 1 - pacotes/auxiliar/auxiliar.go
@@ -1,8 +1,15 @@
 package auxiliar
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/badoux/checkmail"
+)
 
 func Escrever() {
 	fmt.Println("Escrevendo do arquivo aux")
 	escrever2()
+	erro := checkmail.ValidateFormat("teste  naoehvalido")
+
+	fmt.Println(erro)
 }
