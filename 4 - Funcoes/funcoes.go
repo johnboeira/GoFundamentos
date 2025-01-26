a086325a31c9ed8dfdb4f3577e2523808e863dee
johnboeira <johnboeira@gmail.com>
2025-01-26 20:53:23 -0300

    feat: Usando func com 2 retornos

--- 4 - Funcoes/funcoes.go
+++ 4 - Funcoes/funcoes.go
@@ -6,7 +6,25 @@ func somar(n1 int8, n2 int8) int8 {
 	return n1 + n2
 }
 
+func calculosMatematicos(n1, n2 int8) (int8, int8) {
+	soma := n1 + n2
+	subtracao := n1 - n2
+	return soma, subtracao
+}
+
 func main() {
 	soma := somar(10, 30)
 	fmt.Println(soma)
+
+	var f = func(txt string) {
+		fmt.Println(txt)
+	}
+
+	f("Func f")
+
+	//Ignora o segundo valor retornado (subtracao)
+	resultadoAdicao, _ := calculosMatematicos(10, 10)
+
+	fmt.Println(resultadoAdicao)
+
 }
